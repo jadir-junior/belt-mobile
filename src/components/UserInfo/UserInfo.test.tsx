8fd740f1a5c4c7a7115779ab0a85179374200f3c
Jadir Junior <jadirjsjunior@gmail.com>
2021-10-18 15:38:23 -0300

    :test_tube: create a test to user info

--- src/components/UserInfo/UserInfo.test.tsx
+++ src/components/UserInfo/UserInfo.test.tsx
@@ -1,11 +1,37 @@
 import React from 'react'
+import { User } from '../../services/user.service'
 import { UserInfo } from './UserInfo'
 import { render } from '@testing-library/react-native'
 
 describe('UserInfo', () => {
-  it.skip('should render default elements', async () => {
-    const { getByText } = render(<UserInfo />)
+  it('should render default elements', async () => {
+    const { getByLabelText } = render(<UserInfo />)
 
-    expect(getByText(/UserInfo/i)).toBeDefined()
+    expect(getByLabelText(/user information/i)).toBeDefined()
+  })
+
+  it('should render render with name', async () => {
+    const user: User = {
+      _id: '124',
+      email: 'johndoe@email.com',
+      permissionFlags: 1,
+      name: 'John Doe'
+    }
+    const { getByText } = render(<UserInfo {...user} />)
+
+    expect(getByText(/john doe/i)).toBeDefined()
+  })
+
+  it('should render render with position', async () => {
+    const user: User = {
+      _id: '124',
+      email: 'johndoe@email.com',
+      permissionFlags: 1,
+      name: 'John Doe',
+      position: 'CTO'
+    }
+    const { getByText } = render(<UserInfo {...user} />)
+
+    expect(getByText(/cto/i)).toBeDefined()
   })
 })
