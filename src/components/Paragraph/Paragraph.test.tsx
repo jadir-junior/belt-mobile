8f4a9f6d1d82db688f7eca78be6fbf080fed8668
Jadir Junior <jadirjsjunior@gmail.com>
2021-10-18 14:54:51 -0300

    :hammer: refactor paragraph to receive color prop primary

--- src/components/Paragraph/Paragraph.test.tsx
+++ src/components/Paragraph/Paragraph.test.tsx
@@ -8,4 +8,22 @@ describe('Paragraph', () => {
 
     expect(getByText(/im a paragraph/i)).toBeDefined()
   })
+
+  it('should render default color', async () => {
+    const { getByText } = render(<Paragraph>im a paragraph</Paragraph>)
+
+    expect(getByText(/im a paragraph/i)).toHaveStyle({
+      color: '#9E9E9E'
+    })
+  })
+
+  it('should render with a primary color', async () => {
+    const { getByText } = render(
+      <Paragraph color="primary">im a paragraph</Paragraph>
+    )
+
+    expect(getByText(/im a paragraph/i)).toHaveStyle({
+      color: '#040404'
+    })
+  })
 })
